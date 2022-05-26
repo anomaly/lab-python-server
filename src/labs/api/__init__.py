e8116ebbf8dbba4bb8ab7b09c7dcf7a6ea856e41
Dev Mukherjee <devraj@gmail.com>
2022-05-26 18:43:15 +0930

    refactor: proxy support for the api endpoints
    adds parameters to dockerfile to pass on a root path for starlette and
    fastapi to ensure that we can proxy the api endpoints for development
    and production

--- src/labs/api/__init__.py
+++ src/labs/api/__init__.py
@@ -13,7 +13,7 @@
 __name__ = "Labs"
 __version__ = "0.0.1"
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from .routers import router as router_misc
 
@@ -21,9 +21,31 @@ from .routers import router as router_misc
 """
 app = FastAPI(
     title=__name__,
-    root_path="/api",
+    description="Sample API",
     docs_url="/docs",
-    openapi_tags=["misc"],
+    terms_of_service="https://github.com/anomaly/labs",
+    contact={
+      "name": "Anomaly Software",
+      "url": "https://github.com/anomaly/labs",
+      "email": "oss@anomaly.ltd"
+    },
+    license_info={
+      "name": "Apache 2.0",
+      "url": "https://www.apache.org/licenses/LICENSE-2.0"
+    },
+    openapi_tags=[
+      {
+        "name": "auth",
+        "description": "Authentication related endpoints"
+      }
+    ]
     )
 
 app.include_router(router_misc, prefix="/ext")
+
+@app.get("/")
+async def root(request: Request):
+  return {
+    "message": "Welcome to the {} API".format(__name__),
+    "root_path": request.scope.get("root_path")
+  }
