ea532d758123381a1ea0297a2e6506ae0f2f42a0
Dev Mukherjee <devraj@gmail.com>
2022-05-27 12:42:57 +0930

    feat: layout modules for the worker

---
+++ src/labs/worker/tasks/__init__.py
@@ -0,0 +1,6 @@
+"""Tasks for the queue processor
+
+  Create a relevant module for each task and then register
+  then here as demonstrated.
+
+"""
---
+++ src/labs/worker/tasks/void.py
@@ -0,0 +1,6 @@
+"""Defines a task that logs a message
+
+  This is merely a demonstrate how tasks are  meant to be created
+  for Anomaly projects.
+
+"""
