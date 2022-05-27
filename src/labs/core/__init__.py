16f02c9338be42db50dd79d6a2749f26662fc247
Dev Mukherjee <devraj@gmail.com>
2022-05-27 12:43:39 +0930

    refactor: location of package name and version

--- src/labs/core/__init__.py
+++ src/labs/core/__init__.py
@@ -3,7 +3,12 @@
   This package contains components that are shared across the application, e.g:
     - Database model definitions
     - Pydantic model definitions
+    - Database context (async) configured to speak to Postgres
+    - Background worker infrastructure
+
+  Both the worker and API should depend on what's defined in core and the changes
+  made to this package should fundamentally affect the application e.g add a model.
 
 """
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
