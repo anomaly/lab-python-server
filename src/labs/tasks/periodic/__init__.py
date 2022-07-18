f0b371059049a863f24ef2ba935c1cb4a625dc5d
Dev Mukherjee <devraj@gmail.com>
2022-07-18 10:04:24 +0930

    feat: adds example of a periodic task REFS #13
    a basic example of a periodic task that fires every minute, at present does
    nothing more than write to the log a message to the python logger infrasrtructure

---
+++ src/labs/tasks/periodic/__init__.py
@@ -0,0 +1,21 @@
+""" Example of periodic task using Celery
+
+    This is a simple example of a periodic task using Celery.
+    for further documentation please refer to:
+    https://docs.celeryq.dev/en/stable/userguide/periodic-tasks.html
+
+"""
+
+from ...celery import app
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    """ Wireup the tasks that have to be run on a schedule
+    """
+    sender.add_periodic_task(1.0,
+        check_every_so_often.s('a periodic task running'),
+        name='add every minute')
+
+@app.task
+def check_every_so_often(arg):
+    print(arg)
