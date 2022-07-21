0d5aaf63d46928d23fa6d8707307b5db23f3a212
Dev Mukherjee <devraj@gmail.com>
2022-07-21 11:47:45 +0930

    feat: adds convinence methods for generic and user models

--- src/labs/models/user.py
+++ src/labs/models/user.py
@@ -3,14 +3,13 @@
 
 from sqlalchemy import JSON, Boolean, Column, ForeignKey, String
 from sqlalchemy.sql import expression
-from sqlalchemy import update as sqlalchemy_update
 from sqlalchemy.future import select
 from sqlalchemy.exc import NoResultFound
 
 from ..db import Base
-from .utils import DateTimeMixin, IdentifierMixin
+from .utils import DateTimeMixin, IdentifierMixin, ModelCRUDMixin
 
-class User(Base, IdentifierMixin, DateTimeMixin):
+class User(Base, IdentifierMixin, DateTimeMixin, ModelCRUDMixin):
     """
     """
     
@@ -46,6 +45,16 @@ class User(Base, IdentifierMixin, DateTimeMixin):
     @classmethod
     async def get_by_email(cls, session, email):
         query = select(cls).where(cls.email == email)
+        try:
+            results = await session.execute(query)
+            (result,) = results.one()
+            return result
+        except NoResultFound: # noqa: E722
+            return None
+
+    @classmethod
+    async def get_by_phone(cls, session, phone):
+        query = select(cls).where(cls.mobile_number == phone)
         try:
             results = await session.execute(query)
             (result,) = results.one()
--- src/labs/models/utils.py
+++ src/labs/models/utils.py
@@ -5,8 +5,12 @@
 
 """
 
-from sqlalchemy import Column, DateTime, text
+from sqlalchemy import Column, DateTime, ForeignKey, text
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.future import select
+from sqlalchemy import update as sqlalchemy_update,\
+    delete as sqlalchemy_delete
 
 class IdentifierMixin(object):
     """An ID for a given object
@@ -42,3 +46,71 @@ class DateTimeMixin(object):
         onupdate=text("now()"),
         nullable=False,
     )
+
+class CreatedByMixin(object):
+    """ Adds references to created_by and updated_by users
+
+    """
+    @declared_attr
+    def created_by_user_id(cls):
+        return Column(UUID(as_uuid=True),
+            ForeignKey("core.user.id"),
+            nullable=False)
+
+    @declared_attr
+    def last_updated_by_user_id(cls):
+        return Column(UUID(as_uuid=True),
+            ForeignKey("core.user.id"),
+            nullable=False)
+
+
+class ModelCRUDMixin:
+    """
+    A CRUD Mixin designed to abstract CRUD operations for all
+    SQLAlchemy defined models.
+
+    Adapted from the work of Ahmed Nafies:
+    - https://bit.ly/3coTT7j
+    - https://bit.ly/3OmhtPw
+    """
+    @classmethod
+    async def create(cls, **kwargs):
+        async_db_session.add(cls(**kwargs))
+        await async_db_session.commit()
+
+    @classmethod
+    async def update(cls, id, **kwargs):
+        query = (
+            sqlalchemy_update(cls)
+            .where(cls.id == id)
+            .values(**kwargs)
+            .execution_options(synchronize_session="fetch")
+        )
+
+        await async_db_session.execute(query)
+        await async_db_session.commit()
+
+    @classmethod
+    async def get(cls, id):
+        query = select(cls).where(cls.id == id)
+        results = await async_db_session.execute(query)
+        (result,) = results.one()
+        return result
+
+    @classmethod
+    async def get_all(cls):
+        query = select(cls)
+        users = await db.execute(query)
+        users = users.scalars().all()
+        return users
+
+    @classmethod
+    async def delete(cls, id):
+        query = sqlalchemy_delete(cls).where(cls.id == id)
+        await db.execute(query)
+        try:
+            await db.commit()
+        except Exception:
+            await db.rollback()
+            raise
+        return True
