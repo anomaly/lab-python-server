5978ae9c9bc10b45443e2f7b5f81fce5e4605cfb
Dev Mukherjee <devraj@gmail.com>
2022-07-22 09:11:37 +0930

    refactor!: working out ways to run async code in sync

--- src/labs/tasks/otp/__init__.py
+++ src/labs/tasks/otp/__init__.py
@@ -2,25 +2,31 @@
 
 """
 import asyncio
-from asgiref.sync import async_to_sync
 
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+
+# App related imports
 from ...celery import app
-
 from ..utils import send_sms_message, email_sender
+from ...db import get_async_session
+from ...models import User
 
 async def get_user_info(user_id):
-    from ...db import get_async_session
-    from ...models import User
     session = get_async_session()
-    import logging
-    logging.error(session)
-    user = await User.get(await session(), user_id)
-
+    user = await User.get(session, user_id)
+    return user
 
 @app.task()
 def initiate_otp_via_sms(user_id):
+
+    user = loop.run_until_complete(get_user_info(user_id))
+
+    import logging
+    logging.error(user)
+    
     #async_to_sync(get_user_info)(user_id)
-    send_sms_message("00000", "Your OTP is 1234")
+    #send_sms_message("00000", "Your OTP is 1234")
 
 
 @app.task()
