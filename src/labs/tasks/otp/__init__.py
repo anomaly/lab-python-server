11aeba25964bc5991dd71e1e58bfd7bdb7119756
Dev Mukherjee <devraj@gmail.com>
2022-07-21 16:04:16 +0930

    chore: disable call to async function for now

--- src/labs/tasks/otp/__init__.py
+++ src/labs/tasks/otp/__init__.py
@@ -1,15 +1,26 @@
 """ OTP related tasks 
 
 """
-
+import asyncio
+from asgiref.sync import async_to_sync
 
 from ...celery import app
 
 from ..utils import send_sms_message, email_sender
 
+async def get_user_info(user_id):
+    from ...db import get_async_session
+    from ...models import User
+    session = get_async_session()
+    import logging
+    logging.error(session)
+    user = await User.get(await session(), user_id)
+
+
 @app.task()
 def initiate_otp_via_sms(user_id):
-    send_sms_message("00000", "Your OTP is " + user_id)
+    #async_to_sync(get_user_info)(user_id)
+    send_sms_message("00000", "Your OTP is 1234")
 
 
 @app.task()
