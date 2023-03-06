ac0795f81b021b6e1a81b0b4b4e2a0fb5d15b6f0
Dev Mukherjee <devraj@gmail.com>
2023-03-07 08:26:16 +1100

    docs: adds notes on healthcheck response

--- src/labs/schema/ext.py
+++ src/labs/schema/ext.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from pydantic import BaseModel, validator
 
 class HealthCheckResponse(BaseModel):
-    """
-    
+    """ Provides a health check response with a timestamp
+
+    All cloud native applications should provide a health check endpoint
+    for loadbalancers to see if the application is healthy
     """
 
     all_ok: bool = False
