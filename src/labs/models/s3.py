7269b340cec087f5c025c1a9c303e38b2974db4b
Dev Mukherjee <devraj@gmail.com>
2023-04-17 10:39:24 +1000

    refactor: moves s3 filestore to annotated models

--- src/labs/models/s3.py
+++ src/labs/models/s3.py
@@ -7,35 +7,42 @@ from typing import TYPE_CHECKING, Union
 
 from sqlalchemy import Boolean, Column, ForeignKey, String, Integer
 from sqlalchemy.sql import expression
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 
-from .utils import DateTimeMixin, IdentifierMixin, ModelCRUDMixin
+from .utils import DateTimeMixin, IdentifierMixin, ModelCRUDMixin, fk_user_uuid
 from ..utils import minio_client
 from ..config import config
 
-class S3FileMetadata(DateTimeMixin, IdentifierMixin, ModelCRUDMixin):
-    """
-    
+class S3FileMetadata(
+    DateTimeMixin,
+    IdentifierMixin,
+    ModelCRUDMixin
+):
+    """ S3 File object storage and usage pattern.
+
+    This model is used to store metadata about files stored in an S3 compatible
+    object store, and then let various parts of your application link to 
+    the objects for application specific use.
+
+    e.g. user has uploaded identification documents.
+
     """
 
     __tablename__ = "s3_file_metadata"
 
-    s3_key = Column(String,
-        nullable=False)
-    prefix = Column(String,
-        default="",
-        nullable=False)
+    s3_key: Mapped[str]
+    prefix: Mapped[str]
 
-    file_name = Column(String,
-        nullable=False)
-    file_size = Column(Integer,
-        nullable=False)
-    mime_type = Column(String,
-        nullable=False)
+    file_name: Mapped[str]
+    file_size: Mapped[int]
 
-    deleted = Column(Boolean,
-        server_default=expression.false(),
-        nullable=False)
+    mime_type: Mapped[str]
+
+    deleted: Mapped[bool] = mapped_column(
+        default=False
+    )
+
+    user_id: Mapped[fk_user_uuid]
 
     user = relationship(
         "User",
@@ -45,6 +52,8 @@ class S3FileMetadata(DateTimeMixin, IdentifierMixin, ModelCRUDMixin):
 
     @property
     def signed_download_url(self) -> Union[str, None]:
+        """
+        """
         try:
             return minio_client.presigned_get_object(
                 config.S3_BUCKET_NAME,
@@ -68,3 +77,25 @@ class S3FileMetadata(DateTimeMixin, IdentifierMixin, ModelCRUDMixin):
             return url
         except Exception:
             return None
+        
+    def enable_legal_hold(self) -> bool:
+        try:
+            minio_client.put_object_legal_hold(
+                config.S3_BUCKET_NAME,
+                self.s3_key,
+                True
+            )
+            return True
+        except Exception:
+            return False
+        
+    def disable_legal_hold(self) -> bool:
+        try:
+            minio_client.put_object_legal_hold(
+                config.S3_BUCKET_NAME,
+                self.s3_key,
+                False
+            )
+            return True
+        except Exception:
+            return False
