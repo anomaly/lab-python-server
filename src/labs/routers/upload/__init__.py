20edd939969306fbbef4b4cf9b73e2fcd3d32d1b
Dev Mukherjee <devraj@gmail.com>
2023-04-18 10:13:24 +1000

    refactor: requires an authenticated user to upload files

--- src/labs/routers/upload/__init__.py
+++ src/labs/routers/upload/__init__.py
@@ -6,8 +6,11 @@ from datetime import datetime
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+# Made available by the router utils
+from ..utils import get_current_user
+
 from ...db import get_async_session
-from ...models import S3FileMetadata
+from ...models import S3FileMetadata, User
 from ...schema import FileUploadRequest, FileUploadResponse
 
 router = APIRouter(tags=["file-uploads"])
@@ -15,16 +18,18 @@ router = APIRouter(tags=["file-uploads"])
 @router.post("")
 async def get_upload_url(
     upload_request: FileUploadRequest,
-    session: AsyncSession = Depends(get_async_session)
+    session: AsyncSession = Depends(get_async_session),
+    current_user: User = Depends(get_current_user),
 ) -> FileUploadResponse:
     """
     """
 
     s3_file_metadata = await S3FileMetadata.create(
         session,
-        title=upload_request.file_name,
-        size=upload_request.file_size,
+        file_name=upload_request.file_name,
+        file_size=upload_request.file_size,
         mime_type=upload_request.mime_type,
+        user=current_user,
     )
 
     response = FileUploadResponse(
