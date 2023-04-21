36636c74805c33353137171cd4b26abf5ae41f29
Dev Mukherjee <devraj@gmail.com>
2023-04-21 12:43:51 +1000

    refactor: drops redis list broker
    moving to using inmemory broker just for the sake of getting this to work
    taskiq recommend using python pika and rabbitmq for the broker and redis
    to write the result backend

--- src/labs/broker.py
+++ src/labs/broker.py
@@ -4,15 +4,6 @@ from .config import config
 # https://pypi.org/project/taskiq-redis/
 from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend
 
-redis_async_result = RedisAsyncResultBackend(
-    redis_url=config.redis_dsn,
-)
+from taskiq import InMemoryBroker
 
-# TaskIQ have two brokers with similar interfaces, but with different logic. 
-# The PubSubBroker uses redis' pubsub mechanism and is very powerful, 
-# but it executes every task on all workers, because PUBSUB broadcasts message 
-# to all subscribers.
-broker = ListQueueBroker(
-    url=config.redis_dsn,
-    result_backend=redis_async_result,
-)
+broker = InMemoryBroker()
