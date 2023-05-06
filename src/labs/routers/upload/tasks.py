de55a04aa102424336214822ef4791a1d1ac49fb
Dev Mukherjee <devraj@gmail.com>
2023-05-06 16:37:41 +1000

    feat: stub for file availability background task

---
+++ src/labs/routers/upload/tasks.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from taskiq import TaskiqDepends
+
+from ...db import get_async_session
+from ...utils import minio_client
+from ...broker import broker
+
+
+@broker.task
+async def verify_s3_file_availability(
+    s3_file_metadata_id: UUID,
+    session: AsyncSession = TaskiqDepends(get_async_session)
+) -> None:
+    pass
