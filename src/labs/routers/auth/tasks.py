53dc920e753d8eb073207feb00ff306d36f4f394
Dev Mukherjee <devraj@gmail.com>
2023-05-08 09:16:09 +1000

    feat: adds otp sending via email

--- src/labs/routers/auth/tasks.py
+++ src/labs/routers/auth/tasks.py
@@ -5,6 +5,7 @@ from taskiq import TaskiqDepends
 from ...db import get_async_session
 from ...email import sender
 from ...broker import broker
+from ...config import config
 
 from ...models import User
 
@@ -14,6 +15,22 @@ async def send_reset_password_email(
     session: AsyncSession = TaskiqDepends(get_async_session)
 ) -> None:
     user = await User.get(session, user_id)
+
+    # Create a verification code, this is available
+    # only at the time of calling this 
+    reset_password_token = await user.get_verification_token(session)
+
+    sender.send(
+        receivers=[user.email],
+        subject="Your verification email",
+        text_template="email_reset_password_token.txt",
+        html_template="email_reset_password_token.html",
+        body_params={
+            "domain": "google.com",
+            "password_reset_code": reset_password_token,
+            "user": user,
+        },
+    )
 
 @broker.task
 async def send_account_verification_email(
@@ -25,7 +42,7 @@ async def send_account_verification_email(
 
     # Create a verification code, this is available
     # only at the time of calling this 
-    verification_code = await user.get_verification_token(session)
+    verification_token = await user.get_verification_token(session)
 
     sender.send(
         receivers=[user.email],
@@ -34,7 +51,7 @@ async def send_account_verification_email(
         html_template="email_verify_account.html",
         body_params={
             "domain": "google.com",
-            "verification_code": verification_code,
+            "verification_code": verification_token,
             "user": user,
         },
     )
@@ -48,7 +65,7 @@ async def send_welcome_email(
 
 
 @broker.task
-async def sent_otp_sms(
+async def send_otp_sms(
     user_id: UUID,
     session: AsyncSession = TaskiqDepends(get_async_session)
 ) -> None:
@@ -59,6 +76,28 @@ async def send_otp_email(
     user_id: UUID,
     session: AsyncSession = TaskiqDepends(get_async_session)
 ) -> None:
+    """
+    Generates the OTP for a user and email it to them
+
+    This is almost identical to the send_otp_sms task
+    except that it sends the OTP via email instead of SMS.
+    """
     user = await User.get(session, user_id)
+
+    otp = user.get_otp(
+        digits = config.APP_TOTP_NUM_DIGITS,
+        timeout = config.APP_TOTP_INTERVAL,
+    )
+
+    sender.send(
+        receivers=[user.email],
+        subject="Your one time password",
+        text_template="email_otp.txt",
+        html_template="email_otp.html",
+        body_params={
+            "otp": otp,
+            "user": user,
+        },
+    )
 
 
