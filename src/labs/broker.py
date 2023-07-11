ee9e93afe2a43f26ee0b983dbdaf576f76a0686f
Dev Mukherjee <devraj@gmail.com>
2023-07-11 12:41:03 +1000

    refactor: taskiq moves up to 0.8.0

--- src/labs/broker.py
+++ src/labs/broker.py
@@ -6,11 +6,11 @@ from .settings import settings
 
 # See PyPI for more information about taskiq_redis
 # https://pypi.org/project/taskiq-redis/
-# 
+#
 # Redis is recommended as the results backend for the tasks
 from taskiq_redis import RedisAsyncResultBackend
 
-# RabbitMQ is recommended as the broker 
+# RabbitMQ is recommended as the broker
 from taskiq_aio_pika import AioPikaBroker
 
 # Task Scheduler for periodic tasks
@@ -36,7 +36,7 @@ broker = (
 
 # Override the broker to use the InMemory backend
 # if FastAPI is being called via pytest
-#if settings.env == "pytest":
+# if settings.env == "pytest":
 # from taskiq import InMemoryBroker
 # broker = InMemoryBroker()
 
@@ -52,7 +52,9 @@ broker.add_middlewares(
 
 scheduler = TaskiqScheduler(
     broker=broker,
-    sources=[LabelScheduleSource(broker)],
+    sources=[
+        LabelScheduleSource(broker)
+    ],
 )
 
 # The middleware is used to inject the broker into FastAPI
