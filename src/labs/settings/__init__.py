1cb2d8400c52a5cebc9771a8b7dc7a4f2b73e8ba
Dev Mukherjee <devraj@gmail.com>
2023-07-19 13:20:19 +1000

    chore: prettier fixes

--- src/labs/settings/__init__.py
+++ src/labs/settings/__init__.py
@@ -14,6 +14,7 @@ from .jwt import JWTSettings
 from .api_router import APIRouterSettings
 from .verbosity import VerbositySettings
 
+
 class Settings(BaseSettings):
     """Configuration for the application
 
@@ -64,7 +65,6 @@ class Settings(BaseSettings):
     # Verbosity of various tokens
     verbosity: VerbositySettings = VerbositySettings()
 
-    
+
 # A singleton instance of the configuration
 settings: Settings = Settings()
-
