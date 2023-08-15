a8ebc5586eb20c5861029dcd8b08508cb78f01eb
Dev Mukherjee <devraj@gmail.com>
2023-08-15 14:27:27 +1000

    refactor: operation_id generation the fastapi100 way

--- src/labs/api.py
+++ src/labs/api.py
@@ -23,26 +23,44 @@ from .broker import broker
 
 from .schema.ext import RootResponse
 
+
+def generate_operation_id(route: APIRoute) -> str:
+    """
+        With a little help from FastAPI docs
+        https://bit.ly/3rXeAvH
+
+        Globally use the path name as the operation id thus
+        making things a lot more readable, note that this requires
+        you name your functions really well.
+
+        Read  more about this on the FastAPI docs
+        https://shorturl.at/vwz03
+    """
+    return route.name
+
+
 """A FastAPI application that serves handlers
 """
 app = FastAPI(
-  title=__title__,
-  version=__version__,
-  description=settings.api_router.__doc__,
-  docs_url=settings.api_router.path_docs,
-  root_path=settings.api_router.path_root,
-  terms_of_service=settings.api_router.terms_of_service,
-  contact=settings.api_router.contact,
-  license_info=settings.api_router.license_info,
-  openapi_tags=settings.api_router.open_api_tags
+    title=__title__,
+    version=__version__,
+    description=settings.api_router.__doc__,
+    docs_url=settings.api_router.path_docs,
+    root_path=settings.api_router.path_root,
+    terms_of_service=settings.api_router.terms_of_service,
+    contact=settings.api_router.contact,
+    license_info=settings.api_router.license_info,
+    openapi_tags=settings.api_router.open_api_tags,
+    generate_unique_id_function=generate_operation_id,
 )
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-  await websocket.accept()
-  while True:
-    data = await websocket.receive_text()
-    await websocket.send_text(f"Message text was: {data}")
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
 
 
 # Additional routers of the application described in the routers package
@@ -56,42 +74,24 @@ async def app_startup():
         await broker.startup()
 
 # On shutdown, we need to shutdown the broker
+
+
 @app.on_event("shutdown")
 async def app_shutdown():
     if not broker.is_worker_process:
         await broker.shutdown()
 
 # Default handler
+
+
 @app.get(
     "/",
     status_code=status.HTTP_200_OK,
 )
 async def root(request: Request) -> RootResponse:
-  """ Placeholder for the root endpoint
-  """
-  return RootResponse(
-    message="Welcome to the {} API".format(__name__),
-    root_path=request.scope.get("root_path")
-  )
-
-# Hook up any events worth responding to
-# https://fastapi.tiangolo.com/advanced/events/
-
-# With a little help from FastAPI docs
-# https://bit.ly/3rXeAvH
-#
-# Globally use the path name as the operation id thus
-# making things a lot more readable, note that this requires
-# you name your functions really well.
-def use_route_names_as_operation_ids(app: FastAPI) -> None:
-  """
-  Simplify operation IDs so that generated API clients have simpler function
-  names.
-
-  Should be called only after all routes have been added.
-  """
-  for route in app.routes:
-    if isinstance(route, APIRoute):
-      route.operation_id = route.name
-
-use_route_names_as_operation_ids(app)
+    """ Placeholder for the root endpoint
+    """
+    return RootResponse(
+        message="Welcome to the {} API".format(__name__),
+        root_path=request.scope.get("root_path")
+    )
